9813f8152ec88db3dcd73bb9187c7eec99bb67f7
Daniel Plohmann (Saturn) <daniel.plohmann@mailbox.org>
2022-11-04 12:45:15 +0100

    fixed block table percentage

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="mcritweb",
-    version="0.9.3",
+    version="0.9.4",
     packages=["mcritweb"],
     include_package_data=True,
     install_requires=[
