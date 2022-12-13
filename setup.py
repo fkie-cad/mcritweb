a0a280504582903a39c956c27d7807b8c7e0c727
pnx@pyrite <daniel.plohmann@fkie.fraunhofer.de>
2022-12-13 16:46:54 +0100

    bump 0.9.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="mcritweb",
-    version="0.9.6",
+    version="0.9.7",
     packages=["mcritweb"],
     include_package_data=True,
     install_requires=[
