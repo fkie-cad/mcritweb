9973fd29a8559d7e44f2ae03c0f76d9d53f381a2
Daniel Plohmann (Saturn) <daniel.plohmann@mailbox.org>
2023-02-17 17:36:36 +0100

    bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="mcritweb",
-    version="0.10.1",
+    version="0.10.2",
     packages=["mcritweb"],
     include_package_data=True,
     install_requires=[
