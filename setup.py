37506306c2985a91109b2dfa6430a0d034c6285b
pnx@pyrite <daniel.plohmann@fkie.fraunhofer.de>
2023-10-02 14:43:36 +0200

    pin to 2.2.5?

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     packages=["mcritweb"],
     include_package_data=True,
     install_requires=[
-        "flask==2.2.2",
-        "werkzeug==2.2.2",
+        "flask==2.2.5",
+        "werkzeug==2.2.5",
         "flask-dropzone",
         "Pillow",
         "numpy",
